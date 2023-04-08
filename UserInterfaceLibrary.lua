3c742f454e5cfd3c3743429544d029abd29ca410
natnatzsx <130044609+nat-xyz-management@users.noreply.github.com>
2023-04-08 15:14:30 +0800

    Update UserInterfaceLibrary.lua

--- UserInterfaceLibrary.lua
+++ UserInterfaceLibrary.lua
@@ -29,7 +29,7 @@ local Library = {
     HudRegistry = {};
 
     FontColor = Color3.fromRGB(255, 255, 255);
-    MainColor = Color3.fromRGB(28, 28, 28);
+    MainColor = Color3.fromRGB(118, 0, 208);
     BackgroundColor = Color3.fromRGB(20, 20, 20);
     AccentColor = Color3.fromRGB(244, 95, 209);
     OutlineColor = Color3.fromRGB(50, 50, 50);
